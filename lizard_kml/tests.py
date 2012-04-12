7add4e338e61ebfe1472bb7cc2374c8cdd5239e8
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-04-12 14:12:38 +0200

    fixed the tests; run buildout

--- lizard_kml/tests.py
+++ lizard_kml/tests.py
@@ -1,22 +1,31 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
+from django.test.client import Client
+import nose.tools as nt
 
-from django.test import TestCase
+def test():
+    assert True
 
+class TestView(object):
+    def setup(self):
+        self.client = Client()
 
-class ViewsTest(TestCase)
     # Create tests for all urls.
-    def test_index(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
-    def test_kml(self):
-        resp = self.client.get('/kml')
-        self.assertEqual(resp.status_code, 200)
-    def test_kml(self):
-        resp = self.client.get('/kml/lod')
-        self.assertEqual(resp.status_code, 200)
-    def test_kml(self):
-        resp = self.client.get('/kml/overview')
-        self.assertEqual(resp.status_code, 200)
-    def test_kml(self):
-        resp = self.client.get('/kml/transect/7003800')
-        self.assertEqual(resp.status_code, 200)
+    def test_viewer(self):
+        resp = self.client.get('/kml/viewer/')
+        assert resp.status_code == 200
+
+    def test_kml1(self):
+        resp = self.client.get('/kml/kml/')
+        assert resp.status_code != 200
+        
+    def test_kml2(self):
+        resp = self.client.get('/kml/kml/lod/')
+        assert resp.status_code != 200
+        
+    def test_kml3(self):
+        resp = self.client.get('/kml/kml/overview/')
+        assert resp.status_code != 200
+        
+    def test_kml4(self):
+        resp = self.client.get('/kml/kml/transect/7003800/')
+        assert resp.status_code != 200
