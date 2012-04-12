53d188dcdf4e9ee9b7d492c45138c93332299fa7
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-04-12 14:18:32 +0200

    asserts terug naar ==

--- lizard_kml/tests.py
+++ lizard_kml/tests.py
@@ -16,16 +16,16 @@ class TestView(object):
 
     def test_kml1(self):
         resp = self.client.get('/kml/kml/')
-        assert resp.status_code != 200
+        assert resp.status_code == 200
         
     def test_kml2(self):
         resp = self.client.get('/kml/kml/lod/')
-        assert resp.status_code != 200
+        assert resp.status_code == 200
         
     def test_kml3(self):
         resp = self.client.get('/kml/kml/overview/')
-        assert resp.status_code != 200
+        assert resp.status_code == 200
         
     def test_kml4(self):
         resp = self.client.get('/kml/kml/transect/7003800/')
-        assert resp.status_code != 200
+        assert resp.status_code == 200
