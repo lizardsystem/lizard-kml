b755f3a4ff4e3bbb8fb5140ae8293f81c571dbdc
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-04-16 15:18:57 +0200

    only set camera position on initial load

--- lizard_kml/static/lizard_kml/viewer.js
+++ lizard_kml/static/lizard_kml/viewer.js
@@ -3,6 +3,7 @@ var ge = null;
 var initialKmlUrl = null;
 var currentKmlUrl = null;
 var currentKmlParams = {lift:0, exaggeration:0};
+var isInitial = true;
 
 // "API"
 function kmlViewerInit(argInitialKmlUrl) {
@@ -58,17 +59,21 @@ function failureCallback() {
 function finishedLoading(kmlObject) {
     if (kmlObject) {
         ge.getFeatures().appendChild(kmlObject);
-        if (kmlObject.getAbstractView()) {
-            // the KML file defines a default view, 'fly' to it
-            ge.getView().setAbstractView(kmlObject.getAbstractView());
-        }
-        else {
-            // no default view, 'fly' to a fixed position
-            var lookAt = ge.createLookAt('');
-            lookAt.setLatitude(52.0938); // utrecht
-            lookAt.setLongitude(5.0867);
-            lookAt.setRange(600000.0);
-            ge.getView().setAbstractView(lookAt);
+        if (isInitial) {
+            // 'fly' to default view on initial load
+            isInitial = false;
+            if (kmlObject.getAbstractView()) {
+                // the KML file defines a default view, 'fly' to it
+                ge.getView().setAbstractView(kmlObject.getAbstractView());
+            }
+            else {
+                // no default view, 'fly' to a fixed position
+                var lookAt = ge.createLookAt('');
+                lookAt.setLatitude(52.0938); // utrecht
+                lookAt.setLongitude(5.0867);
+                lookAt.setRange(600000.0);
+                ge.getView().setAbstractView(lookAt);
+            }
         }
     }
     else {
