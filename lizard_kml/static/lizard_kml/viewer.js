f216249a69e3d3c08b396dc130ae4187dab85dac
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-04-16 14:57:30 +0200

    javascript sliders for lift and exaggeration

--- lizard_kml/static/lizard_kml/viewer.js
+++ lizard_kml/static/lizard_kml/viewer.js
@@ -1,22 +1,13 @@
 google.load('earth', '1');
 var ge = null;
 var initialKmlUrl = null;
+var currentKmlUrl = null;
+var currentKmlParams = {lift:0, exaggeration:0};
 
+// "API"
 function kmlViewerInit(argInitialKmlUrl) {
     initialKmlUrl = argInitialKmlUrl;
     google.earth.createInstance('map3d', initCallback, failureCallback);
-}
-
-function kmlViewerLoadKml(kmlUrl) {
-    if (ge) {
-        // remove currently loaded features
-        var features = ge.getFeatures();
-        while (features.getFirstChild())
-           features.removeChild(features.getFirstChild());
-    
-        // have the plugin download and parse the requested KML file
-        google.earth.fetchKml(ge, kmlUrl, finishedLoading);
-    }
 }
 
 function initCallback(pluginInstance) {
@@ -24,6 +15,40 @@ function initCallback(pluginInstance) {
     ge.getWindow().setVisibility(true);
 
     kmlViewerLoadKml(initialKmlUrl);
+}
+
+// "API"
+function kmlViewerLoadKml(kmlUrl) {
+    if (ge) {
+        currentKmlUrl = kmlUrl;
+        updateKml();
+    }
+}
+
+// "API"
+function kmlViewerSetLift(lift) {
+    currentKmlParams['lift'] = lift;
+    updateKml();
+}
+
+// "API"
+function kmlViewerSetExaggeration(exaggeration) {
+    currentKmlParams['exaggeration'] = exaggeration;
+    updateKml();
+}
+
+// TODO: deal with "cancelled" updates, when sliders were adjusted during an update
+function updateKml() {
+    if (ge && currentKmlUrl && currentKmlParams) {
+        // remove currently loaded features
+        var features = ge.getFeatures();
+        while (features.getFirstChild()) {
+            features.removeChild(features.getFirstChild());
+        }
+        // have the plugin download and parse the requested KML file
+        var url  = currentKmlUrl + '?' + jQuery.param(currentKmlParams);
+        google.earth.fetchKml(ge, url, finishedLoading);
+    }
 }
 
 function failureCallback() {
