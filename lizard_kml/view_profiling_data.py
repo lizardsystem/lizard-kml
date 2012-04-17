4db449b269a373de7aeb8e98125b4262e027a3d4
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-04-17 10:27:16 +0200

    should test for __main__

--- lizard_kml/view_profiling_data.py
+++ lizard_kml/view_profiling_data.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-
 import hotshot.stats
 import sys
 
-stats = hotshot.stats.load(sys.argv[1])
-#stats.strip_dirs()
-stats.sort_stats('time', 'calls')
-stats.print_stats(20)
+if __name__ == '__main__':
+    stats = hotshot.stats.load(sys.argv[1])
+    #stats.strip_dirs()
+    stats.sort_stats('time', 'calls')
+    stats.print_stats(20)
