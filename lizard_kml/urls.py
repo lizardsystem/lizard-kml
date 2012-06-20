89e72f2638c63bf05668a97934f88bdce6901213
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-06-20 18:08:28 +0200

    remove /kml/ so urls are determined by "parent" site config; add JSON API urls;

--- lizard_kml/urls.py
+++ lizard_kml/urls.py
@@ -5,20 +5,33 @@ from django.conf.urls.defaults import url
 from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
+
 from lizard_kml.views import *
+from lizard_kml.jarkus.views import *
+from lizard_kml.api_drf import *
+from lizard_kml.api_tp import *
 
 admin.autodiscover()
+
+category_resource = CategoryResource()
+kmlsource_resource = KmlSourceResource()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^kml/viewer/(?P<category_id>[0-9]*)/$', ViewerView.as_view(), name='lizard-kml-viewer'),
-    url(r'^kml/viewer/$', ViewerView.as_view(), name='lizard-kml-viewer'),
-    url(r'^kml/info/(?P<id>[0-9]+)/$', InfoView.as_view(), name='lizard-kml-info'),
-    url(r'^kml/xls/(?P<id>[0-9]+)/$', XlsView.as_view(), name='lizard-kml-xls'),
-    url(r'^kml/chart/(?P<chart_type>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/$', ChartView.as_view(), name='lizard-kml-chart'),
-    url(r'^kml/kml/(?P<kml_type>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/$', KmlView.as_view(), name='lizard-kml-kml'),
-    url(r'^kml/kml/(?P<kml_type>[-a-zA-Z0-9_]+)/$', KmlView.as_view(), name='lizard-kml-kml'),
+    url(r'^viewer/$', ViewerView.as_view(), name='lizard-kml-viewer'),
+    url(r'^jarkuskml/(?P<kml_type>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/$', JarkusKmlView.as_view(), name='lizard-jarkus-kml'),
+    url(r'^jarkuskml/(?P<kml_type>[-a-zA-Z0-9_]+)/$', JarkusKmlView.as_view(), name='lizard-jarkus-kml'),
+    url(r'^info/(?P<id>[0-9]+)/$', InfoView.as_view(), name='lizard-kml-info'),
+    url(r'^xls/(?P<id>[0-9]+)/$', XlsView.as_view(), name='lizard-kml-xls'),
+    url(r'^chart/(?P<chart_type>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/$', ChartView.as_view(), name='lizard-kml-chart'),
+    #url(r'^kml/(?P<kml_type>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/$', KmlView.as_view(), name='lizard-kml-kml'),
+    #url(r'^kml/(?P<kml_type>[-a-zA-Z0-9_]+)/$', KmlView.as_view(), name='lizard-kml-kml'),
+    url(r'^kml/(?P<kml_resource_id>[0-9]+)/$', KmlResourceView.as_view(), name='lizard-kml-kml'),
+
+    url(r'^api_tp/', include(category_resource.urls)),
+    url(r'^api_tp/', include(kmlsource_resource.urls)),
+    url(r'^api_drf/$', CategoryTreeView.as_view(), name='api-category-tree'),
 )
 
 urlpatterns += debugmode_urlpatterns()
