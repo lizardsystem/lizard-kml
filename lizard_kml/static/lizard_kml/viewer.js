dd8b58567cbf18201a88c3f4db9367a5dc247e54
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-06-29 14:43:19 +0200

    only show preview image on image load

--- lizard_kml/static/lizard_kml/viewer.js
+++ lizard_kml/static/lizard_kml/viewer.js
@@ -619,8 +619,15 @@ KmlViewerUi.prototype.initJarkusPanel = function () {
 /**
  */
 KmlViewerUi.prototype.showPreviewImage = function (url) {
-    $('#kml-preview').attr('src', url);
-    $('#kml-preview-container').show();
+    var $preview = $('#kml-preview');
+    $preview.load(function() {
+        // only continue if the users mouse is still on the element
+        // for which the preview is shown
+        if ($(this).attr('src') == url) {
+            $('#kml-preview-container').show();
+        }
+    });
+    $preview.attr('src', url);
 };
 
 /**
