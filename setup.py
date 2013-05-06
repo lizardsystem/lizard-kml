835421937cbc9a2d047677d2baf2d1071caa0ee8
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-05-06 13:45:05 +0200

    fix deps

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ install_requires = [
     'pandas >= 0.9.1, < 1.0',
     'numpy',
     'scipy >= 0.10.0, < 0.11',
+    'statsmodels >= 0.4.3, < 0.5',
     'netCDF4 >= 1.0, < 2.0',
     'xlwt >= 0.7.4, < 0.8',
     ],
